df201b5da592952d5c92a0052f43b5a4f032c547
Rich Alossi <alossix@gmail.com>
2023-07-28 10:37:29 +0200

    feat: setup of packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+// eslint-disable-next-line
+const nextTranslate = require("next-translate-plugin");
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    emotion: true,
+  },
+  ...nextTranslate(),
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/api/sitemap",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
